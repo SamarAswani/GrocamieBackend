21dd0fe32a03895c9d898c9ce9b9995a3f70c7a6
SamarAswani <44739967+SamarAswani@users.noreply.github.com>
2022-11-02 01:02:39 +0000

    Update product.js

--- routes/product.js
+++ routes/product.js
@@ -37,50 +37,7 @@ router.post("/productById", async (req, res) => {
   }
 });
 
-// router.get("/get", async (req, res) => {
-//   // const errors = validationResult(req);
-//   // if (!errors.isEmpty()) {
-//   //   return res.status(400).json({
-//   //     errors: errors.array()
-//   //   });
-//   // }
-//
-//   const {search, category, sortKey, sortOrder, itemsPerPage, pageNo}= req.body;
-//
-//   var skips = itemsPerPage * (pageNo - 1)
-//
-//   let dbReq;
-//   if (!(search == null || search == "") && !(category == null || category == "")) {
-//     var searchSplit = search.split(" ");
-//     dbReq = Product.find({keywords: {$in: searchSplit}, category: category});
-//
-//   }
-//
-//   else if (!(category == "" || category == null)){
-//     dbReq = Product.find({keywords: category});
-//   }
-//
-//   else {
-//     var searchSplit = search.split(" ");
-//     dbReq = Product.find({keywords: {$in: searchSplit}});
-// }
-// // If sortKEy present
-// if (!(sortKey == null || sortKey == "")){
-//   var query = {};
-//   query[sortKey] = sortOrder;
-//   dbReq = dbReq.sort(query).skip(skips).limit(itemsPerPage);
-//   dbReq.then(oProduct => {
-//     res.send(oProduct);
-//   }).catch(err => {
-//     res.status(500).send({
-//       message: err.message || "Some error occurred while retrieving the product."
-//     });
-//   })
-// }
-// else {
-//   res.send(dbReq)
-// }
-// });
+
 
 router.post("/get", async(req, res) => {
 
@@ -429,25 +386,7 @@ catch(e){
 
 });
 
-// try {
-//   const product = await Product.aggregate(
-//     [
-//       {$match: { $text: { $search: search }, availability: "Y"}},
-//       {$addFields: {sortValue:{$add: [{$meta: "textScore"}, {$divide: ["$PriorityIndex", 10]}]}}},
-//       {$sort: {sortValue:-1}}
-//       { $match: { sortValue: { $gt: 0.5 } } }
-//     ]
-//     );
-//
-//     res.json(product);
-//
-//   }
-//
-//   catch (e){
-//     console.log(e);
-//   }
-//
-// });
+
 
 router.post("/getOld", async (req, res) => {
   // const errors = validationResult(req);
